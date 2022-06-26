5b766c7adafcbc9e6e48d912266b5c687e877cd8
davidyoon <qqpo12@naver.com>
2022-06-26 17:06:36 +0900

    add collectionView for tag Collection

--- TagCollectionView/Scene/ViewController.swift
+++ TagCollectionView/Scene/ViewController.swift
@@ -14,17 +14,63 @@ class ViewController: UIViewController {
         return view
     }()
 
+    private lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.showsHorizontalScrollIndicator = false
+        return collectionView
+    }()
+
+    private let tagList = ["WWDC", "Apple", "iPhone", "개발자", "판교", "리뉴얼 앱", "한국투자증권", "알고리즘"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return tagList.count
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+
+
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(width: 70.0, height: collectionView.frame.height)
+    }
+}
+
+
 private extension ViewController {
     func setupView() {
         view.backgroundColor = .systemBackground
         [
-            topView
+            topView,
+            collectionView
         ]
             .forEach {
                 view.addSubview($0)
@@ -33,6 +79,12 @@ private extension ViewController {
         topView.snp.makeConstraints {
             $0.top.leading.trailing.equalToSuperview()
             $0.height.equalTo(100.0)
+        }
+
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(topView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(50.0)
         }
     }
 }
